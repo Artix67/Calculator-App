26b5a6ec951226fe14f90e8af7b728b594a1f894
Artix67 <Robert.sanders67@gmail.com>
2023-07-16 15:53:41 -0500

    built the base for CalculatorProcessor class

--- SWE_App/CalculatorProcessor.cpp
+++ SWE_App/CalculatorProcessor.cpp
@@ -1,1 +1,13 @@
 #include "CalculatorProcessor.h"
+#include "wx/wx.h"
+#include <string>
+
+CalculatorProcessor::CalculatorProcessor() {
+
+}
+
+CalculatorProcessor::~CalculatorProcessor() {
+
+}
+
+
--- SWE_App/CalculatorProcessor.h
+++ SWE_App/CalculatorProcessor.h
@@ -1,5 +1,15 @@
 #pragma once
+#include "wx/wx.h"
+#include <string>
+
 class CalculatorProcessor
 {
+	CalculatorProcessor();
+	~CalculatorProcessor();
+
+public:
+
+
+
 };
 
